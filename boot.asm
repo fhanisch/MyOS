d81aa8b6b834270489149b5b0298371265d0571e
fh <fh@pi3.com>
2017-01-21 00:10:40 +0100

    Textausgabe erweitert

--- boot.asm
+++ boot.asm
@@ -3,41 +3,10 @@
 
 	jmp main
 
-cls:	;void cls() --> Bildschirm löschen
-	mov ax, 0xb800		;Adresse Bildschirmspeicher
-	mov es, ax
-	mov di, 3999
-cls1:
-	mov byte [es:di], 0
-	dec di
-	dec di
-	cmp di,1
-	jne cls1
-	ret
-
-print:	;void print(char *str, int x, int y)
-	mov ax, 0xb800
-	mov es, ax
-	mov si, msg
-	mov ax, 0
-	mov di, ax
-print1:
-	mov al, [ds:si]
-	or al, al
-	jz print2
-	mov byte [es:di], al
-	inc di
-	mov byte [es:di], 2
-	inc di
-	inc si
-	jmp print1
-print2:
-	ret
-
-
+%include "screen.asm"
 
 main:
-	cli			;interrupts blockieren
+	cli				;interrupts blockieren
 
 	;stack initialisieren
 	mov ax, 0x07c0
@@ -45,17 +14,40 @@ main:
 	mov ax, 0x9000
 	mov ss, ax
 	mov sp, 0
-	sti			;interrupts zulassen
+	sti				;interrupts zulassen
 
-	call cls		;cls()
+	;cls()
+	call cls
+
+	;print(hello,0,0,grün)
+	push 2			;color
+	push 0			;y
+	push 0			;x
+	push hello		;str
+	call print
+
+	;print(tag,100,6,blau)
+	push 1			;color
+	push 6			;y
+	push 100		;x
+	push tag		;str
+	call print
+
+	;print(msg,0,20,?)
+	push 3			;color
+	push 20			;y
+	push 0			;x
+	push msg		;str
 	call print
 
 main1:
 	jmp main1
 
 ;daten
-msg	db '*** Hello World !!! ***', 0
-	db 'My Operating System || 18.01.2017', 0
-	times 510-($-$$) db 0
-	db 0x55, 0xAA
+hello	db '*** Hello World !!! ***', 0
+tag		db 'Guten Tag !', 0
+msg		db 'Das ist das Haus vom Nikolaus !!!!!!', 0
+		db 'My Operating System || 18.01.2017', 0
+		times 510-($-$$) db 0
+		db 0x55, 0xAA
 
---
+++ screen.asm
@@ -0,0 +1,42 @@
+;void cls() --> Bildschirm löschen
+cls:
+	mov ax, 0xb800		;Adresse Bildschirmspeicher
+	mov es, ax
+	mov di, 3999
+cls1:
+	mov byte [es:di], 0
+	dec di
+	dec di
+	cmp di, 1
+	jne cls1
+	ret
+
+;void print(char *str, int x, int y, int color)
+print:
+	pop dx			;ip auf cx
+	pop si
+	pop bx
+	pop cx
+	pop ax
+	push dx
+	mov byte [ds:color], al
+	mov ax, 0xb800
+	mov es, ax
+	mov ax, 160
+	mul cx
+	add ax, bx
+	mov di, ax
+print1:
+	mov al, [ds:si]
+	or al, al
+	jz print2
+	mov byte [es:di], al
+	inc di
+	mov byte al, [ds:color]
+	mov byte [es:di], al
+	inc di
+	inc si
+	jmp print1
+print2:
+	ret
+color	db 0
